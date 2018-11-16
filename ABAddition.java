744652932a5b1cd95bff084bef49cf799447294c
RyanPham-001 <44855019+RyanPham-001@users.noreply.github.com>
2018-11-15 19:13:38 -0500

    Create ABAddition.java

---
+++ ABAddition.java
@@ -0,0 +1,21 @@
+*/
+A+B
+Given two integer,   A and B.
+Their sum needs to be calculated.
+*/
+
+
+import java.util.*;
+
+public class ABAddition \\Can only work with whole numbers
+{
+  public static void main (String args[]) 
+  {
+  Scanner UserInput = new Scanner(System.in);
+  System.out.println("Enter the first number");
+  int FirstNumber = UserInput.nextInt(); \\Takes the first number that the user inputs
+  System.out.println("Enter the second number");
+  int SecondNumber = UserInput.nextInt(); \\ Takes the second number that the user inputs
+  System.out.println("Sum" + (FirstNumber + SecondNumber)); \\Adds the two numbers the user inputs
+  }
+}
