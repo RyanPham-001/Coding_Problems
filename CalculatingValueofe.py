2df2ad2ca88be696e15e96ec4395d1ef24e3f9b5
RyanPham-001 <44855019+RyanPham-001@users.noreply.github.com>
2018-11-20 19:33:32 -0500

    Create CalculatingValueofe.py

---
+++ CalculatingValueofe.py
@@ -0,0 +1,16 @@
+import math
+series_limit = 0
+start = 1
+while(series_limit<9999):
+  start += 1/(factorial(series_limit))
+  
+def factorial (value):
+  if (value ==1):
+    return(1)
+  else:
+   final *= value
+   value--
+   factorial(value)
+return(final)
+  
+  
