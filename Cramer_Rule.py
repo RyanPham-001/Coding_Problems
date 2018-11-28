8cb6ed29ecd3b5aad4de38ecb67b8cbf38eff6c1
RyanPham-001 <44855019+RyanPham-001@users.noreply.github.com>
2018-11-28 14:15:49 -0500

    Create Cramer_Rule.py

---
+++ Cramer_Rule.py
@@ -0,0 +1,10 @@
+# 2w - x + 5y + z = -3
+# 3w + 2x + 2y - 6z = -32
+# w + 3x + 3y -z = -47
+# 5w - 2x - 3y + 3z = 49
+
+number_of_variables = input("How many variables are there? ")
+while (number_of_variables > 0):
+
+
+def system_of values:
