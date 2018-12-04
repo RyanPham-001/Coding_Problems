47d252ea2e8afca19fb0438ac3c0d86dc9eb517a
RyanPham-001 <44855019+RyanPham-001@users.noreply.github.com>
2018-12-04 16:15:43 -0500

    Update CostOfGiven.java

--- CostOfGiven.java
+++ CostOfGiven.java
@@ -3,35 +3,35 @@ Given a length and width and cost of each tile, determine the total cost of putt
 */
 
 import java.util.*;
-
-public CostOfGiven
+import java.text.DecimalFormat;
+public class CostOfGiven
 {
     public static void main (String args[])
     {
         Scanner UserInput = new Scanner(System.in);
-        DecimalFormat ChangeToMoney = new DecimalFormat("0.00");
+        DecimalFormat ChangeToMoney = new DecimalFormat("#.00");
 
-        System.out.println("What is the total length of the floor? ");
+        System.out.print("What is the total length of the floor? ");
         double lengthOfFloor = UserInput.nextDouble(); //Takes the length of the floor from the user
 
-        System.out.println("What is the total widith of the floor? ");
+        System.out.print("\nWhat is the total widith of the floor? ");
         double widthOfFloor = UserInput.nextDouble(); //Takes the width of the floor from the user
 
-        System.out.println("What is the length of the tile? ");
+        System.out.print("\nWhat is the length of the tile? ");
         double lengthOfTile = UserInput.nextDouble(); //Takes the length of the tile from the user
 
-        System.out.println("What is the width of the tile? ");
+        System.out.println("\nWhat is the width of the tile? ");
         double widthOfTile = UserInput.nextDouble(); //Takes the width of the tiles from the user
 
-        System.out.println("What is the cost of each tile? ");
+        System.out.println("\nWhat is the cost of each tile? ");
         double CostOfTile = UserInput.nextDouble(); //Takes the cost of each tile from the user
 
         double AreaOfFloor = Multiplication(lengthOfFloor,widthOfFloor); //Finds the total area of the floor using mutliplication
         double AreaOfTile = Multiplication(lengthOfTile,widthOfTile); //Finds the total area of the tile using multiplication
         double NumberOfTiles = Division(AreaOfFloor,AreaOfTile); //Finds the number of tiles that can fit in the area
 
-        double TotalCost = Multiplication(NumberOfTiles,CostOfTile);
-
+        double TotalCost = Multiplication(NumberOfTiles,CostOfTile); //Finds the total cost of the tiles in the floor
+        System.out.println("The total cost to fill this " + widthOfFloor +" by "+ lengthOfFloor +" floor with tiles is $"+ ChangeToMoney.format(TotalCost));;
     }
 
     public static double Multiplication(double FirstValue, double SecondValue)
@@ -45,6 +45,5 @@ public CostOfGiven
         return(FirstValue/SecondValue); //Returns the quotient of the two given values
     }
 
-    public static double
 
 }
