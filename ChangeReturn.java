3215ca99e633ec74cf6220d2fdf4b36903d31b53
RyanPham-001 <44855019+RyanPham-001@users.noreply.github.com>
2018-12-12 18:07:31 -0500

    Create ChangeReturn.java

---
+++ ChangeReturn.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class ChangeReturn
+{
+public static void main (String args[])
+{
+Scanner UserInput = new Scanner(System.in);
+System.out.print("What is the total cost of the product? ");
+double costOfProduct = UserInput.nextDouble();
+System.out.print("\nWhat is the amount of money that you spent?");
+double paymentofProduct = UserInput.nextDouble();
+
+}
+
+}
